772ca3433f0f255c1f80159068c8d28937e8247b
Narumedsr Pitayachamrat <narumedsr.p@gmail.com>
2026-08-08 14:07:50 +0700

    feat: add mock_users seed script

---
+++ seeds/mock_users.sql
@@ -0,0 +1,13 @@
+TRUNCATE TABLE mock_users;
+
+INSERT INTO mock_users (name)
+VALUES
+  ('Panat Lorchatchawankul'),
+  ('Nitipoom Pansayor'),
+  ('Narumedsr Pitayachamrat'),
+  ('Tanakit Banluprasong'),
+  ('Nuttapong Jinda'),
+  ('Narongwas Benjalux'),
+  ('Keeratikorn Samutrnawin'),
+  ('Kantapon Phutthapratanporn'),
+  ('Kannawich Munsak');
