b9f0105c29b3b42c408f98ff83a8548d80e74f2e
Narongwas Benjalux <darkwar008@gmail.com>
2026-09-08 00:14:27 +0700

    refactor: remove unnecessary Promise.all

--- src/services/orders-branch.service.ts
+++ src/services/orders-branch.service.ts
@@ -4,8 +4,7 @@ import type {
   LotDeduction,
 } from "@/models/orders-branch.model";
 import { db } from "@/db/client";
-import { product } from "@/db/schema/product";
-import { eq, inArray, sum, and, gt, sql } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 import {
   branchOrderAllocation,
   branchOrderDetail,
@@ -32,59 +31,38 @@ export const ordersBranchService = {
     const now = new Date();
 
     return db.transaction(async (tx) => {
-      const [products, productsCount] = await Promise.all([
-        tx.query.product.findMany({
-          columns: {
-            pId: true,
-            name: true,
-            costPrice: true,
-          },
-          where: (product, { inArray }) => inArray(product.pId, pIds),
-          with: {
-            headOrderDetails: {
-              where: (headOrderDetail, { and, gt, exists }) =>
-                and(
-                  gt(headOrderDetail.expiredDate, now),
-                  exists(
-                    tx
-                      .select({ lotId: order.lotId })
-                      .from(order)
-                      .where(
-                        and(
-                          eq(order.lotId, headOrderDetail.lotId),
-                          eq(order.status, "approved"),
-                          gt(headOrderDetail.available, 0),
-                        ),
+      const products = await tx.query.product.findMany({
+        columns: {
+          pId: true,
+          name: true,
+          costPrice: true,
+        },
+        where: (product, { inArray }) => inArray(product.pId, pIds),
+        with: {
+          headOrderDetails: {
+            where: (headOrderDetail, { and, gt, exists }) =>
+              and(
+                gt(headOrderDetail.expiredDate, now),
+                exists(
+                  tx
+                    .select({ lotId: order.lotId })
+                    .from(order)
+                    .where(
+                      and(
+                        eq(order.lotId, headOrderDetail.lotId),
+                        eq(order.orderType, "hq"),
+                        eq(order.status, "approved"),
+                        gt(headOrderDetail.available, 0),
                       ),
-                  ),
+                    ),
                 ),
-              orderBy: (headOrderDetail, { asc }) =>
-                asc(headOrderDetail.expiredDate),
-            },
+              ),
+            orderBy: (headOrderDetail, { asc }) =>
+              asc(headOrderDetail.expiredDate),
           },
-        }),
-        tx
-          .select({
-            pId: product.pId,
-            sumAmount: sum(headOrderDetail.available),
-          })
-          .from(product)
-          .leftJoin(headOrderDetail, eq(product.pId, headOrderDetail.pId))
-          .leftJoin(order, eq(headOrderDetail.lotId, order.lotId))
-          .where(
-            and(
-              inArray(product.pId, pIds),
-              eq(order.status, "approved"),
-              gt(headOrderDetail.expiredDate, now),
-              gt(headOrderDetail.available, 0),
-            ),
-          )
-          .groupBy(product.pId),
-      ]);
+        },
+      });
 
-      const productCountMap = new Map(
-        productsCount.map((item) => [item.pId, Number(item.sumAmount ?? 0)]),
-      );
       const productMap = new Map(products.map((item) => [item.pId, item]));
 
       const [newOrder] = await tx
@@ -95,18 +73,21 @@ export const ordersBranchService = {
         })
         .returning();
 
-      const updatedHQStock: Promise<unknown>[] = [];
-
       const hodIdsMapping: Omit<LotDeduction, "bodId">[][] = [];
 
       const newBOD = items.items.map((item) => {
-        const productHQSum = productCountMap.get(item.pId) ?? 0;
         const product = productMap.get(item.pId);
         if (!product) {
           throw new AppError("NOT_FOUND", {
             message: "Product not found",
           });
         }
+
+        const productHQSum = product.headOrderDetails.reduce(
+          (sum, hod) => sum + hod.available,
+          0,
+        );
+
         if (productHQSum < item.amount) {
           throw new AppError("INSUFFICIENT_STOCK", {
             message: `Insufficient stock for product ${product?.name}`,
@@ -122,14 +103,6 @@ export const ordersBranchService = {
         product.headOrderDetails.forEach((hod) => {
           if (item.amount <= 0) return;
           const deductAmount = Math.min(hod.available, item.amount);
-          updatedHQStock.push(
-            tx
-              .update(headOrderDetail)
-              .set({
-                available: sql`${headOrderDetail.available} - ${deductAmount}`,
-              })
-              .where(eq(headOrderDetail.hodId, hod.hodId)),
-          );
           item.amount -= deductAmount;
           expiredDates.push(hod.expiredDate);
           hodIds.push({ hodId: hod.hodId, amount: deductAmount });
@@ -137,29 +110,42 @@ export const ordersBranchService = {
 
         hodIdsMapping.push(hodIds);
 
-        return tx
-          .insert(branchOrderDetail)
-          .values({
-            lotId: newOrder.lotId,
-            pId: item.pId,
-            amount: branchItemAmount,
-            branchId,
-            remain: branchItemAmount,
-            available: branchItemAmount,
-            costPrice: product.costPrice,
-            basePrice: product.headOrderDetails[0].basePrice,
-            expiredDate: expiredDates.reduce(
-              (minDate, date) => (date < minDate ? date : minDate),
-              expiredDates[0],
-            ),
-          })
-          .returning();
+        return {
+          lotId: newOrder.lotId,
+          pId: item.pId,
+          amount: branchItemAmount,
+          branchId,
+          remain: branchItemAmount,
+          available: branchItemAmount,
+          costPrice: product.costPrice,
+          basePrice: product.headOrderDetails[0].basePrice,
+          expiredDate: expiredDates.reduce(
+            (minDate, date) => (date < minDate ? date : minDate),
+            expiredDates[0],
+          ),
+        };
       });
 
-      await Promise.all(updatedHQStock);
-      const branchOrderDetails = await Promise.all(newBOD);
+      const deductions = hodIdsMapping.flat();
 
-      const bodIds = branchOrderDetails.map(([bod]) => bod.bodId);
+      await tx.execute(sql`
+        update ${headOrderDetail}
+        set ${sql.identifier("available")} = ${headOrderDetail.available} - v.deduct
+        from (values ${sql.join(
+          deductions.map(
+            ({ hodId, amount }) => sql`(${hodId}::int, ${amount}::int)`,
+          ),
+          sql`, `,
+        )}) as v(hod_id, deduct)
+        where ${headOrderDetail.hodId} = v.hod_id
+      `);
+
+      const branchOrderDetails = await tx
+        .insert(branchOrderDetail)
+        .values(newBOD)
+        .returning();
+
+      const bodIds = branchOrderDetails.map((bod) => bod.bodId);
 
       const insertedBOAs: LotDeduction[] = hodIdsMapping.flatMap(
         (hodIds, index) => {
@@ -181,7 +167,7 @@ export const ordersBranchService = {
       return {
         ...newOrder,
         items: branchOrderDetails.map((item) => {
-          const { lotId: _, ...branchOrderDetail } = item[0];
+          const { lotId: _, ...branchOrderDetail } = item;
           return branchOrderDetail;
         }),
       };
